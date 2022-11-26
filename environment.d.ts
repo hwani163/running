930e2a049823c4b7d5a5811eca0356e7eeb4fada
Seokhwan Won <hwani164@naver.com>
2022-11-26 20:05:28 +0900

    a

---
+++ environment.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    STRAVA_CLIENT_ID: string;
+    STRAVA_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_SECRET: string;
+  }
+}
